aee953ad4356e7e226f50cdcf367eb3768324eb4
fahim07ms <162901623+fahim07ms@users.noreply.github.com>
2024-03-14 17:37:50 +0600

    Update shirt.py

--- pshirt/shirt.py
+++ pshirt/shirt.py
@@ -1,5 +1,5 @@
 import sys
-from   PIL import Image
+from   PIL import Image, ImageOps
 
 def main():
     #Check arg
@@ -7,9 +7,22 @@ def main():
 
     try:
         before = Image.open(sys.argv[1])
-        after = Image.open(sys.argv[2])
+       
     except OSError:
         sys.exit("Invalid input")
+    except FileNotFoundError:
+    	sys.exit("File not found")
+    	
+    shirt = Image.open("shirt.png")
+    
+    crpd = ImageOps.fit(shirt, size=(before.size[0], shirt.size[1]))
+    
+    after = Image.new(mode=before.mode, size=before.size)
+ 
+    after.paste(crpd)
+    after.paste(before, box=(0, before.size[1]))
+    
+    after.save(sys.argv[2], format=before.format)
 
     
 
