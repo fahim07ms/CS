694e2a2344f4d7a02d795daebe967dcd0d6ca560
fahim07ms <fahim.mun07@proton.me>
2024-03-15 11:48:35 +0600

    Created folders

--- test_twttr.py
+++
@@ -1,9 +0,0 @@
-from twttr import shorten
-
-def main():
-	test_shorten()
-
-def test_shorten():
-	assert shorten("Twitter") == "Twttr"
-	assert shorten("65") == "65"
-	assert shorten("fAhIm") == "fhm"
